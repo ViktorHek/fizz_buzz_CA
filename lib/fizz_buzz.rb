93e6e3d4f01303f688c8d994a61e256f581dc240
viktor <viktorkarlsson125@gmail.com>
2020-10-09 21:27:23 +0200

    lyckades få in fizz & buzz på rätt numer men istället för att ditt svar visas så visas alla numer mellan 1-100. gjorde rspec, 0 examples & 0 failures

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,36 @@
 
-def fizz_buzz(number)
-    number
+#def fizzbuzz(n)
+#    (1..n).each do |i|
+ #     if i % 3 == 0 && i % 5 == 0
+  #      puts 'fizzbuzz'
+   #   elsif i % 3 == 0
+    #    puts 'fizz'
+     # elsif i % 5 == 0
+      #  puts 'buzz'
+      #else
+       # puts i
+      #end
+    #end
+  #end
+  
+  #fizzbuzz(100)
+
+  #puts "Hello and welcome to Fizz Buzz. the rules are simple: type a number between 1 and 100 and see what you gett!! have fun kidz"
+  #print "enter a number between 1 and 100: "
+
+puts "please pick a number between 1 and 100 "
+number = gets.chomp
+
+1. upto(100) do |number|
+   if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+   elsif number % 3 == 0
+    puts "Fizz" 
+   elsif number % 5 == 0
+    puts "Buzz"
+   elsif 
+    puts number
+   end
 end
+
+
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,9 +1,5 @@
 require "./lib/fizz_buzz"
 
 describe "fizz_buss" do
-    
-    it "Hello and welcomm to fizz buzz" do
-    expect(fizz_buzz|1|).to eq 1
-    end
 
 end
