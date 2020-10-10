9090fa7dc4f79f664a3f1b315a75581a0615c839
viktor <viktorkarlsson125@gmail.com>
2020-10-11 00:34:05 +0200

    något fel i rspec, spelet funkar men får ändå 4 exempel o 4 fel

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,5 +1,21 @@
-require "./lib/fizz_buzz"
 
-describe "fizz_buss" do
+require './lib/fizz_buzz'
 
-end
+describe 'fizz_buzz' do
+    it "returns 1 if number is 1" do
+      expect(fizz_buzz(1)).to eq 1 
+    end
+  
+    it "returns 'That's a Fizz' if number is divisible by 3" do
+      expect(fizz_buzz(6)).to eq "That's a Fizz"
+    end
+  
+    it "returns 'That's a Buzz' if number is divisible by 5" do
+      expect(fizz_buzz(10)).to eq "That's a Buzz"
+    end
+  
+    it "returns fizz buzz if number is divisible by 15" do
+      expect(fizz_buzz(30)).to eq "Congratulations! That's a FizzBuzz! can U find another?"
+    end
+  
+  end
